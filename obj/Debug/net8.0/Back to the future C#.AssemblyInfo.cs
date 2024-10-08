a2b3cccd6588b4da77c4b9e403a8e4ce9903f42e
Kabanizza <romanvodyanitskiy2003@gmail.com>
2024-10-09 00:29:37 +0200

    add a 2 files

--- obj/Debug/net8.0/Back to the future C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/Back to the future C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back to the future C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+904c9a21f1ca686bcb7f4ae416fb0e1cfe734251")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f07b4bdb3e142a1d14e05f1e14a7f38a536c8403")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back to the future C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back to the future C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
